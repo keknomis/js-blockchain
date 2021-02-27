046ad71dc4f0646b8643db877dc44cca991e06f7
Simon <simon.serbetar@equaleyes.com>
2021-02-27 21:36:47 +0100

    proof of work added

--- main.js
+++ main.js
@@ -13,19 +13,32 @@ class Block {
         this.data = data;
         this.previousHash = previousHash;
         this.hash = this.calculateHash();
+        this.nonce = 0;
     }
 
     /*
     Uses the imported SHA256 func.
      */
     calculateHash() {
-        return SHA256(this.index + this.previousHash + this.timestamp + JSON.stringify(this.data)).toString();
+        return SHA256(this.index + this.previousHash + this.timestamp + JSON.stringify(this.data) + this.nonce).toString();
+    }
+
+    mineBlock(difficulty) {
+        // trick
+        while (this.hash.substring(0, difficulty) !== Array(difficulty + 1).join("0")) {
+            this.nonce++;
+            this.hash = this.calculateHash();
+
+        }
+
+        console.log("Block mined: " + this.hash);
     }
 }
 
 class Blockchain {
     constructor() {
         this.chain = [this.createGenesisBlock()];
+        this.diffiuclty = 4;
     }
 
     createGenesisBlock() {
@@ -42,19 +55,21 @@ class Blockchain {
         //sets the previous hash of the new block to the last block on the chain
         newBlock.previousHash = this.getLatestBlock().hash;
         //recalculate its hash - every time a block property gets changed, we must rehash the block
-        newBlock.hash = newBlock.calculateHash();
+        //newBlock.hash = newBlock.calculateHash();
+        newBlock.mineBlock(this.diffiuclty)
         this.chain.push(newBlock);
     }
-    isChainValid(){
-        for(let i=1; i<this.chain.length;i++){
-            const currentBlock =this.chain[i];
-            const previousBlock= this.chain[i-1];
+
+    isChainValid() {
+        for (let i = 1; i < this.chain.length; i++) {
+            const currentBlock = this.chain[i];
+            const previousBlock = this.chain[i - 1];
 
             //is the hash of the current block not equal to current hash
-            if(currentBlock.hash !=currentBlock.calculateHash()){
+            if (currentBlock.hash !== currentBlock.calculateHash()) {
                 return false;
             }
-            if(currentBlock.previousHash !==previousBlock.hash){
+            if (currentBlock.previousHash !== previousBlock.hash) {
                 return false;
             }
         }
@@ -64,15 +79,9 @@ class Blockchain {
 
 let smolyCoin = new Blockchain();
 
-smolyCoin.addBlock(new Block(1,"28/02/2017", {amount:2}));
-smolyCoin.addBlock(new Block(1,"28/02/2017", {amount:2}));
 
-
-console.log('Is valid'+ smolyCoin.isChainValid());
-
-smolyCoin.chain[1].data = {amount :100};
-
-
-console.log('Is valid'+ smolyCoin.isChainValid());
-
+console.log('Mining block 1...');
+smolyCoin.addBlock(new Block(1, "28/02/2017", {amount: 2}));
+console.log('Mining block 2...');
+smolyCoin.addBlock(new Block(1, "28/02/2017", {amount: 2}));
 
